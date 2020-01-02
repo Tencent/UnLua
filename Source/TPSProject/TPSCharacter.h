e9ba597aa8554481aff388a9fdbb5cc410a3424c
rowechien <chienluo@tencent.com>
2020-01-02 14:36:31 +0800

    add TPSCharacter for testing

---
+++ Source/TPSProject/TPSCharacter.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "TPSCharacter.generated.h"
+
+UCLASS()
+class TPSPROJECT_API ATPSCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ATPSCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
