7f1f0587aa38eb11d7cac6c34410f4a3a721fc4a
xuyanghuang <xuyanghuang@tencent.com>
2021-10-19 22:00:06 +0800

    增加：更多的ArrayAPI测试

--- Plugins/UnLua/Source/UnLua/Private/Specs/LuaLib_Array.spec.cpp
+++ Plugins/UnLua/Source/UnLua/Private/Specs/LuaLib_Array.spec.cpp
@@ -106,9 +106,9 @@ void FUnLuaLibArraySpec::Define()
         });
     });
 
-    Describe(TEXT("获取TArray长度"), [this]
+    Describe(TEXT("Length"), [this]
     {
-        It(TEXT("Length"), EAsyncExecution::ThreadPool, [this]()
+        It(TEXT("正确获取长度"), EAsyncExecution::ThreadPool, [this]()
         {
             const char* Chunk = "\
             local Array = UE4.TArray(0)\
@@ -130,6 +130,191 @@ void FUnLuaLibArraySpec::Define()
             ";
             UnLua::RunChunk(L, Chunk);
             TEST_EQUAL(lua_tointeger(L, -1), 2LL);
+        });
+    });
+
+    Describe(TEXT("Add"), [this]
+    {
+        It(TEXT("追加元素到数组末尾"), EAsyncExecution::ThreadPool, [this]()
+        {
+            const char* Chunk = "\
+            local Array = UE4.TArray(0)\
+            Array:Add(1)\
+            Array:Add(2)\
+            return Array\
+            ";
+            UnLua::RunChunk(L, Chunk);
+            const FScriptArray* ScriptArray = UnLua::GetArray(L, -1);
+            TEST_TRUE(ScriptArray!=nullptr);
+
+            const TArray<int32>* Array = (TArray<int32>*)ScriptArray;
+            TEST_EQUAL(Array->Num(), 2);
+            TEST_EQUAL(Array->operator[](0), 1);
+            TEST_EQUAL(Array->operator[](1), 2);
+        });
+    });
+
+    Describe(TEXT("AddUnique"), [this]
+    {
+        It(TEXT("只追加不重复的元素"), EAsyncExecution::ThreadPool, [this]()
+        {
+            const char* Chunk = "\
+            local Array = UE4.TArray(0)\
+            Array:AddUnique(1)\
+            Array:AddUnique(1)\
+            return Array\
+            ";
+            UnLua::RunChunk(L, Chunk);
+            const FScriptArray* ScriptArray = UnLua::GetArray(L, -1);
+            TEST_TRUE(ScriptArray!=nullptr);
+
+            const TArray<int32>* Array = (TArray<int32>*)ScriptArray;
+            TEST_EQUAL(Array->Num(), 1);
+            TEST_EQUAL(Array->operator[](0), 1);
+        });
+    });
+
+    Describe(TEXT("Find"), [this]
+    {
+        It(TEXT("找到元素，返回索引位置"), EAsyncExecution::ThreadPool, [this]()
+        {
+            const char* Chunk = "\
+            local Array = UE4.TArray(0)\
+            Array:Add(1)\
+            Array:Add(2)\
+            return Array:Find(2)\
+            ";
+            UnLua::RunChunk(L, Chunk);
+            TEST_EQUAL(lua_tointeger(L, -1), 2LL);
+        });
+
+        It(TEXT("没有找到元素，返回0"), EAsyncExecution::ThreadPool, [this]()
+        {
+            const char* Chunk = "\
+            local Array = UE4.TArray(0)\
+            Array:Add(1)\
+            Array:Add(2)\
+            return Array:Find(100)\
+            ";
+            UnLua::RunChunk(L, Chunk);
+            TEST_EQUAL(lua_tointeger(L, -1), 0LL);
+        });
+    });
+
+    Describe(TEXT("Insert"), [this]
+    {
+        It(TEXT("插入元素到指定位置"), EAsyncExecution::ThreadPool, [this]()
+        {
+            const char* Chunk = "\
+            local Array = UE4.TArray(0)\
+            Array:Add(1)\
+            Array:Add(2)\
+            Array:Insert(3, 2)\
+            return Array\
+            ";
+            UnLua::RunChunk(L, Chunk);
+            const FScriptArray* ScriptArray = UnLua::GetArray(L, -1);
+            TEST_TRUE(ScriptArray!=nullptr);
+
+            const TArray<int32>* Array = (TArray<int32>*)ScriptArray;
+            TEST_EQUAL(Array->Num(), 3);
+            TEST_EQUAL(Array->operator[](1), 3);
+            TEST_EQUAL(Array->operator[](2), 2);
+        });
+    });
+
+    Describe(TEXT("Remove"), [this]
+    {
+        It(TEXT("移除指定索引处的元素"), EAsyncExecution::ThreadPool, [this]()
+        {
+            const char* Chunk = "\
+            local Array = UE4.TArray(0)\
+            Array:Add(1)\
+            Array:Add(2)\
+            Array:Remove(2)\
+            return Array\
+            ";
+            UnLua::RunChunk(L, Chunk);
+            const FScriptArray* ScriptArray = UnLua::GetArray(L, -1);
+            TEST_TRUE(ScriptArray!=nullptr);
+
+            const TArray<int32>* Array = (TArray<int32>*)ScriptArray;
+            TEST_EQUAL(Array->Num(), 1);
+            TEST_EQUAL(Array->operator[](0), 1);
+        });
+    });
+
+    Describe(TEXT("RemoveItem"), [this]
+    {
+        It(TEXT("移除指定元素"), EAsyncExecution::ThreadPool, [this]()
+        {
+            const char* Chunk = "\
+            local Array = UE4.TArray(0)\
+            Array:Add(1)\
+            Array:Add(2)\
+            Array:RemoveItem(1)\
+            return Array\
+            ";
+            UnLua::RunChunk(L, Chunk);
+            const FScriptArray* ScriptArray = UnLua::GetArray(L, -1);
+            TEST_TRUE(ScriptArray!=nullptr);
+
+            const TArray<int32>* Array = (TArray<int32>*)ScriptArray;
+            TEST_EQUAL(Array->Num(), 1);
+            TEST_EQUAL(Array->operator[](0), 2);
+        });
+    });
+
+    Describe(TEXT("Clear"), [this]
+    {
+        It(TEXT("清除数组，移除所有元素"), EAsyncExecution::ThreadPool, [this]()
+        {
+            const char* Chunk = "\
+            local Array = UE4.TArray(0)\
+            Array:Add(1)\
+            Array:Add(2)\
+            Array:Clear()\
+            return Array\
+            ";
+            UnLua::RunChunk(L, Chunk);
+            const FScriptArray* ScriptArray = UnLua::GetArray(L, -1);
+            TEST_TRUE(ScriptArray!=nullptr);
+
+            const TArray<int32>* Array = (TArray<int32>*)ScriptArray;
+            TEST_EQUAL(Array->Num(), 0);
+        });
+    });
+
+    Describe(TEXT("Reserve"), [this]
+    {
+        It(TEXT("预留N个数组元素的空间"), EAsyncExecution::ThreadPool, [this]()
+        {
+            const char* Chunk = "\
+            local Array = UE4.TArray(0)\
+            local Result = Array:Reserve(10)\
+            return Result, Array\
+            ";
+            UnLua::RunChunk(L, Chunk);
+            TEST_TRUE(lua_toboolean(L, -2));
+
+            const FScriptArray* ScriptArray = UnLua::GetArray(L, -1);
+            TEST_TRUE(ScriptArray!=nullptr);
+
+            const TArray<int32>* Array = (TArray<int32>*)ScriptArray;
+            TEST_EQUAL(Array->GetAllocatedSize(), (SIZE_T)10 * 4);
+        });
+
+        It(TEXT("只能够对空数组进行预留空间操作"), EAsyncExecution::ThreadPool, [this]()
+        {
+            AddExpectedError(TEXT("TArray_Reserve: 'Reserve' is only valid for empty TArray!"));
+            const char* Chunk = "\
+            local Array = UE4.TArray(0)\
+            Array:Add(1)\
+            local Result = Array:Reserve(10)\
+            return Result, Array\
+            ";
+            UnLua::RunChunk(L, Chunk);
+            TEST_FALSE(lua_toboolean(L, -2));
         });
     });
 
