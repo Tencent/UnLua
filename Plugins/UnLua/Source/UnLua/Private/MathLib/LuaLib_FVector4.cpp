1918e93b264106e83f82bdaf8852c98d6cf23355
xuyanghuang <xuyanghuang@tencent.com>
2021-11-02 20:42:12 +0800

    修改：FVector4的构造表达保持和UE一致

--- Plugins/UnLua/Source/UnLua/Private/MathLib/LuaLib_FVector4.cpp
+++ Plugins/UnLua/Source/UnLua/Private/MathLib/LuaLib_FVector4.cpp
@@ -15,25 +15,64 @@
 #include "UnLuaEx.h"
 #include "LuaLib_Math.h"
 
-static int32 FVector4_New(lua_State *L)
+static int32 FVector4_New(lua_State* L)
 {
-    int32 NumParams = lua_gettop(L);
-    void *Userdata = NewTypedUserdata(L, FVector4);
-    FVector4 *V = new(Userdata) FVector4;
-    UnLua::TFieldSetter4<float>::Set(L, NumParams, &V->X);
-    return 1;
+    const int32 NumParams = lua_gettop(L);
+    void* Userdata = NewTypedUserdata(L, FVector4);
+
+    switch (NumParams)
+    {
+    case 1:
+        {
+            new(Userdata) FVector4(ForceInitToZero);
+            return 1;
+        }
+    case 2:
+        {
+            const float& X = lua_tonumber(L, 2);
+            new(Userdata) FVector4(X);
+            return 1;
+        }
+    case 3:
+        {
+            const float& X = lua_tonumber(L, 2);
+            const float& Y = lua_tonumber(L, 3);
+            new(Userdata) FVector4(X, Y);
+            return 1;
+        }
+    case 4:
+        {
+            const float& X = lua_tonumber(L, 2);
+            const float& Y = lua_tonumber(L, 3);
+            const float& Z = lua_tonumber(L, 4);
+            new(Userdata) FVector4(X, Y, Z);
+            return 1;
+        }
+    case 5:
+        {
+            const float& X = lua_tonumber(L, 2);
+            const float& Y = lua_tonumber(L, 3);
+            const float& Z = lua_tonumber(L, 4);
+            const float& W = lua_tonumber(L, 5);
+            new(Userdata) FVector4(X, Y, Z, W);
+            return 1;
+        }
+    }
+
+    UE_LOG(LogUnLua, Log, TEXT("%s: Invalid parameters!"), ANSI_TO_TCHAR(__FUNCTION__));
+    return 0;
 }
 
-static int32 FVector4_Set(lua_State *L)
+static int32 FVector4_Set(lua_State* L)
 {
-    int32 NumParams = lua_gettop(L);
+    const int32 NumParams = lua_gettop(L);
     if (NumParams < 1)
     {
         UE_LOG(LogUnLua, Log, TEXT("%s: Invalid parameters!"), ANSI_TO_TCHAR(__FUNCTION__));
         return 0;
     }
 
-    FVector4 *V = (FVector4*)GetCppInstanceFast(L, 1);
+    FVector4* V = (FVector4*)GetCppInstanceFast(L, 1);
     if (!V)
     {
         UE_LOG(LogUnLua, Log, TEXT("%s: Invalid FVector4!"), ANSI_TO_TCHAR(__FUNCTION__));
@@ -44,35 +83,35 @@ static int32 FVector4_Set(lua_State *L)
     return 0;
 }
 
-static int32 FVector4_UNM(lua_State *L)
+static int32 FVector4_UNM(lua_State* L)
 {
-    FVector4 *V = (FVector4*)GetCppInstanceFast(L, 1);
+    FVector4* V = (FVector4*)GetCppInstanceFast(L, 1);
     if (!V)
     {
         UE_LOG(LogUnLua, Log, TEXT("%s: Invalid FVector4!"), ANSI_TO_TCHAR(__FUNCTION__));
         return 0;
     }
 
-    void *Userdata = NewTypedUserdata(L, FVector4);
+    void* Userdata = NewTypedUserdata(L, FVector4);
     new(Userdata) FVector4(-(*V));
     return 1;
 }
 
 static const luaL_Reg FVector4Lib[] =
 {
-    { "Set", FVector4_Set },
-    { "Add", UnLua::TMathCalculation<FVector4, UnLua::TAdd<float>, true>::Calculate },
-    { "Sub", UnLua::TMathCalculation<FVector4, UnLua::TSub<float>, true>::Calculate },
-    { "Mul", UnLua::TMathCalculation<FVector4, UnLua::TMul<float>, true>::Calculate },
-    { "Div", UnLua::TMathCalculation<FVector4, UnLua::TDiv<float>, true>::Calculate },
-    { "__add", UnLua::TMathCalculation<FVector4, UnLua::TAdd<float>>::Calculate },
-    { "__sub", UnLua::TMathCalculation<FVector4, UnLua::TSub<float>>::Calculate },
-    { "__mul", UnLua::TMathCalculation<FVector4, UnLua::TMul<float>>::Calculate },
-    { "__div", UnLua::TMathCalculation<FVector4, UnLua::TDiv<float>>::Calculate },
-    { "__tostring", UnLua::TMathUtils<FVector4>::ToString },
-    { "__unm", FVector4_UNM },
-    { "__call", FVector4_New },
-    { nullptr, nullptr }
+    {"Set", FVector4_Set},
+    {"Add", UnLua::TMathCalculation<FVector4, UnLua::TAdd<float>, true>::Calculate},
+    {"Sub", UnLua::TMathCalculation<FVector4, UnLua::TSub<float>, true>::Calculate},
+    {"Mul", UnLua::TMathCalculation<FVector4, UnLua::TMul<float>, true>::Calculate},
+    {"Div", UnLua::TMathCalculation<FVector4, UnLua::TDiv<float>, true>::Calculate},
+    {"__add", UnLua::TMathCalculation<FVector4, UnLua::TAdd<float>>::Calculate},
+    {"__sub", UnLua::TMathCalculation<FVector4, UnLua::TSub<float>>::Calculate},
+    {"__mul", UnLua::TMathCalculation<FVector4, UnLua::TMul<float>>::Calculate},
+    {"__div", UnLua::TMathCalculation<FVector4, UnLua::TDiv<float>>::Calculate},
+    {"__tostring", UnLua::TMathUtils<FVector4>::ToString},
+    {"__unm", FVector4_UNM},
+    {"__call", FVector4_New},
+    {nullptr, nullptr}
 };
 
 float Dot3(const FVector4& V1, const FVector4& V2);
@@ -90,4 +129,5 @@ BEGIN_EXPORT_REFLECTED_CLASS(FVector4)
     ADD_NAMED_FUNCTION("ToQuat", ToOrientationQuat)
     ADD_LIB(FVector4Lib)
 END_EXPORT_CLASS()
+
 IMPLEMENT_EXPORTED_CLASS(FVector4)
