c94bd949ea0c7f7c8cbe03b18c409b80350b1d75
xuyanghuang <xuyanghuang@tencent.com>
2021-11-30 19:34:49 +0800

    修正：ISettingsModule.h这些只能在WITH_EDITOR下使用

--- Plugins/UnLua/Source/UnLua/Private/UnLuaModule.cpp
+++ Plugins/UnLua/Source/UnLua/Private/UnLuaModule.cpp
@@ -12,9 +12,12 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
 // See the License for the specific language governing permissions and limitations under the License.
 
+#if WITH_EDITOR
 #include "ISettingsModule.h"
 #include "ISettingsSection.h"
 #include "Modules/ModuleManager.h"
+#endif
+
 #include "LuaContext.h"
 #include "UnLuaSettings.h"
 
