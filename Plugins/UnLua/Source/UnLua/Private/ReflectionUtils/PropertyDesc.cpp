707ea05aded066784930ecc08ce77fa2539a73db
chenguohong <chenguohong@tencent.com>
2022-01-26 17:37:44 +0800

    ENABLE_TYPE_CHECK开启情况下，应该只做类型检查，类型兼容的话支持设置，跟之前的版本保持一致(不能将Object设置为空)

--- Plugins/UnLua/Source/UnLua/Private/ReflectionUtils/PropertyDesc.cpp
+++ Plugins/UnLua/Source/UnLua/Private/ReflectionUtils/PropertyDesc.cpp
@@ -360,15 +360,17 @@ public:
             ObjectBaseProperty->SetObjectPropertyValue(ValuePtr, Value);
         }
         else
-        {   
+        {
+#if ENABLE_TYPE_CHECK == 1
             UObject* Object = UnLua::GetUObject(L, IndexInStack);
             if (Object)
             {
                 if (!Object->GetClass()->IsChildOf(ObjectBaseProperty->PropertyClass))
                 {
-                    Object = nullptr;
+                    UNLUA_LOGERROR(L, LogUnLua, Warning, TEXT("Invalid value type : property.type=%s, value.type=%s"), *ObjectBaseProperty->PropertyClass->GetName(), *Object->GetClass()->GetName());
                 }
             }
+#endif
             ObjectBaseProperty->SetObjectPropertyValue(ValuePtr, Object);
         }
         return true;
