e8222c0b4ba359f180629aafda4c9fe9f1811b79
forsakenyang <forsakenyang@vip.qq.com>
2022-05-30 12:57:48 +0800

    增加：测试用例 #443

---
+++ Content/Script/Tests/Regression/Issue443/TestActor.lua
@@ -0,0 +1,19 @@
+require "UnLua"
+
+local M = Class()
+
+Result = 0
+
+function M:LuaOnly()
+    self:TestEmpty()
+end
+
+function M:CustomEvent()
+    self:LuaOnly()
+    self.Overridden.CustomEvent(self)
+    print("Lua CustomEvent")
+    self:TestEmpty()
+    Result = Result + 1
+end
+
+return M
