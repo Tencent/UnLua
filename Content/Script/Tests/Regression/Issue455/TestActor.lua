975d8d7339c0eb6f01cb1f0c4220500f7cb38140
forsakenyang <forsakenyang@vip.qq.com>
2022-06-14 01:38:06 +0800

    修改：调整测试用例到4.26版本的资源 #455

--- Content/Script/Tests/Regression/Issue455/TestActor.lua
+++ Content/Script/Tests/Regression/Issue455/TestActor.lua
@@ -2,13 +2,16 @@ require "UnLua"
 
 local M = Class()
 
-function M:Test(IntArray,StringArray)
-    print("=======Test ", IntArray,StringArray)
+function M:Test(IntArray, StringArray)
+    print("=======Test ", IntArray, StringArray)
+    Result = ""
     for i=1, IntArray:Num() do
         print("IntArray:", i, IntArray[i])
+        Result = Result .. tostring(IntArray[i])
     end
     for i=1, StringArray:Num() do
         print("StringArray:", i, StringArray[i])
+        Result = Result .. StringArray[i]
     end
 end
 
