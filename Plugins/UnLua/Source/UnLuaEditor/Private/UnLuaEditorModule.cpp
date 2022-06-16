b3f42a16d4199e512ac656a5bbfb2b83674d4c7a
xuyanghuang <xuyanghuang@tencent.com>
2022-06-16 18:58:31 +0800

    增加：ForEachObjectWithPackage兼容4.25

--- Plugins/UnLua/Source/UnLuaEditor/Private/UnLuaEditorModule.cpp
+++ Plugins/UnLua/Source/UnLuaEditor/Private/UnLuaEditorModule.cpp
@@ -12,7 +12,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
 // See the License for the specific language governing permissions and limitations under the License.
 
-#include "UObject/UObjectHash.h"
+#include "Compat/UObjectHash.h"
 #include "UnLuaPrivate.h"
 #include "UnLuaEditorStyle.h"
 #include "UnLuaEditorCommands.h"
