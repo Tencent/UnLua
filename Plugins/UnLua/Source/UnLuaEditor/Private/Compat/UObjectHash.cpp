be71e2c20de0f6a7ca7d03ceac9bed2d088e5616
xuyanghuang <xuyanghuang@tencent.com>
2022-06-20 12:50:20 +0800

    修改：删除多余的#pragma once

--- Plugins/UnLua/Source/UnLuaEditor/Private/Compat/UObjectHash.cpp
+++ Plugins/UnLua/Source/UnLuaEditor/Private/Compat/UObjectHash.cpp
@@ -12,8 +12,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
 // See the License for the specific language governing permissions and limitations under the License.
 
-#pragma once
-
 #include "UObject/UObjectHash.h"
 
 #if ENGINE_MAJOR_VERSION <= 4 && ENGINE_MINOR_VERSION < 26
