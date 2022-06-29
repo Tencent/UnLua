4d684447df4d12cd414d8d299e68b4356568a153
xuyanghuang <xuyanghuang@tencent.com>
2022-06-29 10:12:19 +0800

    修正：4.25下漏掉的include

--- Plugins/UnLua/Source/UnLua/Private/LuaFunction.cpp
+++ Plugins/UnLua/Source/UnLua/Private/LuaFunction.cpp
@@ -13,6 +13,7 @@
 // See the License for the specific language governing permissions and limitations under the License.
 
 
+#include "Engine/BlueprintGeneratedClass.h"
 #include "LuaFunction.h"
 #include "UnLuaModule.h"
 #include "ReflectionUtils/PropertyDesc.h"
