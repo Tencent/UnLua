d4181c6d62ce57d569e08ed3d4244d495e2e7130
xuyanghuang <xuyanghuang@tencent.com>
2022-06-30 17:39:30 +0800

    修正：4.25下的编译错误

--- Plugins/UnLua/Source/UnLua/Private/UnLuaManager.cpp
+++ Plugins/UnLua/Source/UnLua/Private/UnLuaManager.cpp
@@ -12,6 +12,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
 // See the License for the specific language governing permissions and limitations under the License.
 
+#include "Kismet/BlueprintFunctionLibrary.h"
 #include "GameFramework/InputSettings.h"
 #include "Components/InputComponent.h"
 #include "Animation/AnimInstance.h"
