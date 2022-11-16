10888aadc21eaad272b0d4ac46adbb1923907bd0
xuyanghuang <xuyanghuang@tencent.com>
2022-11-16 14:06:56 +0800

    修改：当HotReloadMode为Never时，彻底禁用HotReload模块

--- Plugins/UnLua/Source/UnLua/Private/UnLuaLib.cpp
+++ Plugins/UnLua/Source/UnLua/Private/UnLuaLib.cpp
@@ -50,7 +50,9 @@ namespace UnLua
 
         static int HotReload(lua_State* L)
         {
+#if UNLUA_WITH_HOT_RELOAD
             luaL_dostring(L, "require('UnLua.HotReload').reload()");
+#endif
             return 0;
         }
 
@@ -249,8 +251,14 @@ namespace UnLua
                         end
                     end
                 })
+            )");
+
+#if UNLUA_WITH_HOT_RELOAD
+            luaL_dostring(L, R"(
                 pcall(function() _G.require = require('UnLua.HotReload').require end)
             )");
+#endif
+
             LegacySupport(L);
             return 1;
         }
