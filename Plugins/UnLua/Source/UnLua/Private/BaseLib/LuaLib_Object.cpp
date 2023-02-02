0e933894837b9f72683b4696d5814bff09f83366
xuyanghuang <xuyanghuang@tencent.com>
2023-02-02 17:01:52 +0800

    增加：需要包含的头文件避免UnityBuild导致的编译报错

--- Plugins/UnLua/Source/UnLua/Private/BaseLib/LuaLib_Object.cpp
+++ Plugins/UnLua/Source/UnLua/Private/BaseLib/LuaLib_Object.cpp
@@ -12,6 +12,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
 // See the License for the specific language governing permissions and limitations under the License.
 
+#include "Engine/World.h"
 #include "LowLevel.h"
 #include "UnLuaEx.h"
 #include "LuaCore.h"
